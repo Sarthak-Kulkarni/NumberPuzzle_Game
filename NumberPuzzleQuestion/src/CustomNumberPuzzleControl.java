a1ce8a019e30251efbadd061aed325c491d0b981
Sarthak-Kulkarni <sarthak3050@gmail.com>
2021-07-08 23:33:10 +0530

    Solved the three questions

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -2,10 +2,10 @@ import java.awt.*;
 
 class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	public int getWidth() {
-		return 200;
+		return 300;
 	}
 	public int getHeight() {
-		return 250;
+		return 350;
 	}
 	public int getXPosition() {
 		return 200;
@@ -36,7 +36,22 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
 		
-		//Your logic here		
+		//Your logic here
+		
+		//Finding the ID of the button clicked
+		int buttonClickedId=0;
+		for(int i=0;i<=15;i++) {
+			if(buttons[i].getLabel()==buttonClicked.getLabel()) {
+				buttonClickedId=i;
+				break;
+			}
+		}
+		
+		//Swapping only adjacent cells
+		if(Math.abs(buttonClickedId-emptyCellId)==4 || Math.abs(buttonClickedId-emptyCellId)==1)		
+			{swapButton(buttons[emptyCellId],buttonClicked);
+			emptyCellId=buttonClickedId;
+			}
 		
 		return emptyCellId;
 
@@ -45,7 +60,17 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		int arr[] = new int[15];
 		
 		//Your logic here
-		int a = getRandomNumber();
+		for(int i=0;i<15;i++) {
+			int a = getRandomNumber();
+			arr[i]=a%15+1; //Limiting numbers from 1 to 15
+			//Checking for duplication
+			for(int j=0;j<i;j++) {
+				if(arr[i]==arr[j]) {
+					i--;
+					break;
+				}
+			}
+		}
 		
 		
 		return arr;
@@ -55,7 +80,13 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
+		int[] arr=getIntegerArrayOfButtonIds(buttons);
+		for(int i=0;i<15;i++) {
+			if(arr[i]!=i+1) {
+				winner=false;
+				break;
+			}
+		}
 
 		return winner;
 	}
